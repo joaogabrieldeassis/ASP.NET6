f9ab81d831f91ad518a9ed88b6223147c61b9c50
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-05-27 15:47:48 -0300

    Adicionando as validações

--- CRUDeEntityFramework/Blog/ViewModel/EditorCategoryViewModel.cs
+++ CRUDeEntityFramework/Blog/ViewModel/EditorCategoryViewModel.cs
@@ -4,10 +4,12 @@ namespace Blog.ViewModel
 {
     public class EditorCategoryViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [StringLength(80, MinimumLength  = 4 ,ErrorMessage ="O campo deve conter no minimo 4 caracter e no maximo 80")]
         public string Name{ get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [StringLength(80, MinimumLength = 4, ErrorMessage = "O campo deve conter no minimo 4 caracter e no maximo 80")]
         public string Slug{ get; set; }
     }
 }
