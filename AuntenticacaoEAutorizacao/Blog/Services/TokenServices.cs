7f593248efdf02acbf71ece0199953379350641a
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-01 13:57:58 -0300

    Criando a classe services

---
+++ AuntenticacaoEAutorizacao/Blog/Services/TokenServices.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Models;
+
+namespace Blog.Services
+{
+    public class TokenServices
+    {
+        //Dado um usuario, vamos gerar um token apartir desse método
+        public string GenerateToken(User user)
+        {
+            
+        }
+    }
+}
