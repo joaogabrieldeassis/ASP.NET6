2e933b580d5eea2f66432e8ecd02d70d063f20ba
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-02 15:27:19 -0300

    Criando o metodo do login do usuario e gerando o token

--- AuntenticacaoEAutorizacao/Blog/Controllers/AccountController.cs
+++ AuntenticacaoEAutorizacao/Blog/Controllers/AccountController.cs
@@ -1,6 +1,21 @@
-﻿namespace Blog.Controllers
+﻿using Blog.Services;
+using Microsoft.AspNetCore.Mvc;
+namespace Blog.Controllers
 {
-    public class AccountController
+    [ApiController]
+    public class AccountController : ControllerBase
     {
+        private readonly TokenServices _tokenServices;
+        public AccountController(TokenServices tokenServices)
+        {
+            _tokenServices = tokenServices;
+        }
+        [HttpPost("v1/login")]
+        public IActionResult Login()
+        {
+            
+            var token = _tokenServices.GenerateToken(null);
+            return Ok(token);
+        }
     }
 }
