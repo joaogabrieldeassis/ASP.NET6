e7ed381fb688e9168e235b947899e7a02e33fdfe
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-03 14:47:19 -0300

    Criando os serviços

--- AuntenticacaoEAutorizacao/Blog/Program.cs
+++ AuntenticacaoEAutorizacao/Blog/Program.cs
@@ -1,4 +1,6 @@
 using Blog.DataContext;
+using Blog.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 //Desabilitar a validação automatica da minha api
 builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
@@ -6,6 +8,7 @@ builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
     options.SuppressModelStateInvalidFilter = true;
 });   ;
 builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenServices>(); 
 var app = builder.Build();
 
 app.MapControllers();
