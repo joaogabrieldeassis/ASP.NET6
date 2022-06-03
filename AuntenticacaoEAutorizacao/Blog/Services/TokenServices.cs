d2711a48f35a48ff86be346659bf32fabd506733
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-03 14:49:30 -0300

    Adicioando Serviços aos Tokens

--- AuntenticacaoEAutorizacao/Blog/Services/TokenServices.cs
+++ AuntenticacaoEAutorizacao/Blog/Services/TokenServices.cs
@@ -1,20 +1,28 @@
 ﻿using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Blog.Services;
     public class TokenServices
     {
         //Dado um usuario, vamos gerar um token apartir desse método
-
         public string GenerateToken(User user)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-            var tokenDescriptor = new SecurityTokenDescriptor
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
             {
+                new (ClaimTypes.Name, "João Gabriel"),
+                new (ClaimTypes.Role, "adimin"),
+                new ("Fruta", "Banana")
+            }),
                  Expires = DateTime.UtcNow.AddHours(8),
+                
+
                  SigningCredentials = new SigningCredentials
                  (
                   new SymmetricSecurityKey(key),
