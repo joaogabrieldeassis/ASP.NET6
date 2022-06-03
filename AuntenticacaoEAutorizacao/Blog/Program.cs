554984439ec46135222e8b7faf1c0517bf8b3e1f
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-03 15:45:33 -0300

    Configurando a autenticação e autorização

--- AuntenticacaoEAutorizacao/Blog/Program.cs
+++ AuntenticacaoEAutorizacao/Blog/Program.cs
@@ -1,15 +1,39 @@
+using Blog;
 using Blog.DataContext;
 using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+
+
 //Desabilitar a validação automatica da minha api
 builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
-});   ;
+});   
 builder.Services.AddDbContext<BlogDataContext>();
 builder.Services.AddTransient<TokenServices>(); 
 var app = builder.Build();
 
 app.MapControllers();
 app.Run();
+
+//Curso
