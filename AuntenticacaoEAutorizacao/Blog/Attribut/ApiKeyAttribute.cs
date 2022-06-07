b31ff12a143cc883e2f01b4de722023092794b33
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-07 17:21:08 -0300

    Acabando de finalizar o modulo

---
+++ AuntenticacaoEAutorizacao/Blog/Attribut/ApiKeyAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Blog.Attribut;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.HttpContext.Request.Query.TryGetValue(Configuration.Api_key, out var extractedApiKey))
+        {
+            context.Result = new ContentResult()
+            {
+                StatusCode = 401,
+                Content = "ApiKey Não encontrada"
+            };
+        }
+        if (!Configuration.ApiKey.Equals(extractedApiKey))
+        {
+            context.Result = new ContentResult
+            {
+                StatusCode = 403,
+                Content = "Acesso não autorizado"
+            };
+
+        }
+        await next();
+    }
+}
+
--- AuntenticacaoEAutorizacao/Blog/Configuration.cs
+++ AuntenticacaoEAutorizacao/Blog/Configuration.cs
@@ -3,7 +3,9 @@ namespace Blog
 {
     public static class Configuration
     {
-        public static string JwtKey { get; set; } = "AZBFGh8iPoKwXmYV46fUsJ9r";
+        public static string JwtKey  = "AZBFGh8iPoKwXmYV46fUsJ9r";
+        public static string Api_key = "api_Key";
+        public const string ApiKey = "aOGJKP[IJFE354==";
     }
 
 }
