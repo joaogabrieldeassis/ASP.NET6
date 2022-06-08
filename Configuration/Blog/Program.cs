c5bc1876fd49c834814651dad92f613d4225efe9
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-08 15:30:50 -0300

    Finalizando a primeira aula

--- Configuration/Blog/Program.cs
+++ Configuration/Blog/Program.cs
@@ -6,46 +6,68 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-//Configuração da minha Autenticação e autorização
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
 
 
 
-//Desabilitar a validação automatica da minha api
-builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
-{
-    options.SuppressModelStateInvalidFilter = true;
-});   
-builder.Services.AddDbContext<BlogDataContext>();
-builder.Services.AddTransient<TokenServices>(); 
 
 var app = builder.Build();
-Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
-Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
-Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
-
-var smtp = new Configuration.SmtpConfiguration();
-app.Configuration.GetSection("SmtpConfiguration").Bind(smtp);
-Configuration.Smtp = smtp;  
+LoadConfiguration(app);
+ConfigureAuthentication(builder);
+ConfigureMVC(builder);
+ConfigureServices(builder);
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 app.Run();
+
+//Método para configurar o meu appsettings.json
+void LoadConfiguration (WebApplication app)
+{
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+
+    var smtp = new Configuration.SmtpConfiguration();
+    app.Configuration.GetSection("SmtpConfiguration").Bind(smtp);
+    Configuration.Smtp = smtp;
+}
+
+//Método da Configuração da minha Autenticação e autorização
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+//Método para Desabilitar a validação automatica da minha api
+void ConfigureMVC(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+}
+
+//Método para serviços Do banco e Token
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<BlogDataContext>();
+    builder.Services.AddTransient<TokenServices>();
+}
 
 //Curso
