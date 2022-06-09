b214d7963eef29e2d2826e767f08feef174a43fc
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-09 16:33:18 -0300

    Quase lá

--- Configuration/Blog/Services/EmailServices.cs
+++ Configuration/Blog/Services/EmailServices.cs
@@ -5,15 +5,18 @@ namespace Blog.Services
 {
     public class EmailServices
     {
-        public bool Send(string toName, string toEmail,string subject, string body, string fromName = "João Gabriel",string fromEmail = "joaoassisgabriel@gmail.com")
+        public bool Send(string toName, string toEmail,string subject, string body, string fromName = "Mariana", string fromEmail = "marih.pma@gmail.com")
         {
             var stmpClient = new SmtpClient(Configuration.Smtp.Host, Configuration.Smtp.Port);
+
             stmpClient.Credentials = new NetworkCredential(Configuration.Smtp.UserName, Configuration.Smtp.Password);
             stmpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             stmpClient.EnableSsl = true;
 
             var mail = new MailMessage();
-            mail.From = new MailAddress(fromName,fromEmail);
+
+
+            mail.From = new MailAddress(fromEmail,fromName);
             mail.To.Add(new MailAddress(toEmail,toName));
             mail.Subject = subject; 
             mail.Body = body;
