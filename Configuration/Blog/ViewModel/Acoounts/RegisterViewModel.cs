54b413c821d2170ebdc009af8881ec41882b0aa5
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-10 15:34:38 -0300

    Entendendo a pasta wwwroot

--- Configuration/Blog/ViewModel/Acoounts/RegisterViewModel.cs
+++ Configuration/Blog/ViewModel/Acoounts/RegisterViewModel.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Blog.ViewModel
+namespace Blog.ViewModel.Acoounts
 {
     public class RegisterViewModel
     {
         [Required(ErrorMessage = "Nome obrigatorio")]
-        [StringLength(200,ErrorMessage = "Seu nome deve ter no máximo 200 letras")]
-        public string Name{ get; set; }
+        [StringLength(200, ErrorMessage = "Seu nome deve ter no máximo 200 letras")]
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "Email obrigatorio")]
         [EmailAddress(ErrorMessage = "Email invalido")]
-        public string Email{ get; set; }
+        public string Email { get; set; }
     }
 }
--- Configuration/Blog/ViewModel/Categories/EditorCategoryViewModel.cs
+++ Configuration/Blog/ViewModel/Categories/EditorCategoryViewModel.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Blog.ViewModel
+namespace Blog.ViewModel.Categories
 {
     public class EditorCategoryViewModel
     {
         [Required(ErrorMessage = "Este campo é obrigatorio")]
-        [StringLength(80, MinimumLength  = 4 ,ErrorMessage ="O campo deve conter no minimo 4 caracter e no maximo 80")]
-        public string Name{ get; set; }
+        [StringLength(80, MinimumLength = 4, ErrorMessage = "O campo deve conter no minimo 4 caracter e no maximo 80")]
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatorio")]
         [StringLength(80, MinimumLength = 4, ErrorMessage = "O campo deve conter no minimo 4 caracter e no maximo 80")]
-        public string Slug{ get; set; }
+        public string Slug { get; set; }
     }
 }
