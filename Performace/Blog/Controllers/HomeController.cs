04dfb9bb243280d1e93c53211aac07b344fab433
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-13 13:57:43 -0300

    Adicionando os controles

---
+++ Performace/Blog/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Attribut;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        [ApiKey]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
