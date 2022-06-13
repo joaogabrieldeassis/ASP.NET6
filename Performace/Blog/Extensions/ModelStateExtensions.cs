e6861b206b8bd5a4582312b6668c6da2135cd5d9
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-13 14:00:07 -0300

    Adicionando as extenções

---
+++ Performace/Blog/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Blog.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        //Metodo statico para adicionar e retornar a lista de erro que criei no meu EditorCategoryViewModel
+        public static List<string> GetErros(this ModelStateDictionary modelState)
+        {
+            var result = new List<string>();    
+            foreach (var item in modelState.Values)
+            {
+                foreach (var item2 in item.Errors)
+                {
+                    result.Add(item2.ErrorMessage); 
+                }
+            }
+            return result;
+        }
+    }
+}
